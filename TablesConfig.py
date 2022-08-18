a607b4de58afa2dd2bc2226ddf4c45f55544b94e
asouqi <asouqi@atypon.com>
2022-08-19 00:11:19 +0300

    Add Table Demo

--- TablesConfig.py
+++ TablesConfig.py
@@ -23,7 +23,11 @@ class TablesConfig(Config):
     IMAGE_MAX_DIM = 320
 
     # Use smaller anchors because our image and objects are small
-    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)  # anchor side in pixels
+
+    # Ratios of anchors at each cell (width/height)
+    # A value of 1 represents a square anchor, and 0.5 is a wide anchor
+    RPN_ANCHOR_RATIOS = [0.1, 0.3, 0.5, 1]
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
